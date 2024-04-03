544fc99b032b770eca16e85b05eb709d66f5f03e
dessertgomjelly <msj1291@naver.com>
2024-04-03 22:35:55 +0900

    git test

---
+++ springmvc/src/main/java/hello/springmvc/basic/requestmapping/MappingClassController.java
@@ -0,0 +1,15 @@
+package hello.springmvc.basic.requestmapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MappingClassController {
+
+    public String user(){
+        return "get users"
+    }
+
+    public String addUser(){
+        return
+    }
+}
